15ebb59db0c52ca106609ee7b63f4f8d63af974d
aaajjj23 <76922302+aaajjj23@users.noreply.github.com>
2021-01-06 20:33:17 -0800

    Initial commit

---
+++ HelloWorldAJ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldAJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello Allen Jenkins!");
+        }
+    }
+}
